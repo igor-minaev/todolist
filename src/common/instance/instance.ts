1a72def5b640477d396c2464dc15127dabfb0318
Igor Minayeu <igor.minayeu@gmail.com>
2026-06-17 19:46:18 +0300

    start lesson 12

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,11 @@
-import axios from "axios";
-
+import axios from "axios"
 
 const apiKey = "a30f9853-a5e3-4468-8571-cb595f337f01"
 
 export const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1',
-    headers: {
-        Authorization: `Bearer ${token}`,
-        'API-KEY': apiKey
-    }
+  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  headers: {
+    Authorization: `Bearer ${token}`,
+    "API-KEY": apiKey,
+  },
 })
